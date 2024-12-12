34a70064a34ccbaa1eed5a60a3622ed74f694c9e
Karishma019 <karishmaacharya63@gmail.com>
2024-12-12 08:52:09 +0530

    updated

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -23,6 +23,7 @@ export class DashboardComponent {
   totalSavings: number = 0;
   incomePercentage: number = 0;
   expensePercentage: number = 0;
+  monthlyData: { [month: string]: { income: number; expense: number } } = {};
 
   public barChartOptions: ChartOptions = {
     responsive: true,
@@ -36,11 +37,53 @@ export class DashboardComponent {
   };
   public lineChartLabels: string[] = []; // Declare as public
   public lineChartData: ChartDataset[] = [
-    { data: [], label: 'Income' },
-    { data: [], label: 'Expense' },
+    {
+      data: [],
+      label: 'Income',
+      backgroundColor: ['#2196f3', '#ec4899', '#b6ea9e', '#4caf50', '#66d8e2'],
+    },
+    {
+      data: [],
+      label: 'Expense',
+      backgroundColor: [
+        '#ff8c8c',
+        '#66d8e2',
+        '#4caf50',
+        '#b9ba11',
+        '#b9ba11',
+        '#eaff00',
+        '#eab99e',
+        '#e69eea',
+      ],
+    },
   ];
 
-  // Pie Chart
+  public doughChartOption: ChartOptions = {
+    responsive: true,
+  };
+  public doughnutChartLabels: string[] = []; // Declare as public
+  public doughnutChartData: ChartDataset[] = [
+    {
+      data: [],
+      label: 'Income',
+      backgroundColor: ['#2196f3', '#ec4899', '#b6ea9e', '#4caf50', '#66d8e2'],
+    },
+    {
+      data: [],
+      label: 'Expense',
+      backgroundColor: [
+        '#ff8c8c',
+        '#66d8e2',
+        '#4caf50',
+        '#b9ba11',
+        '#b9ba11',
+        '#eaff00',
+        '#eab99e',
+        '#e69eea',
+      ],
+    },
+  ];
+
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
@@ -75,24 +118,64 @@ export class DashboardComponent {
       this.totalExpense = expense;
       this.totalSavings = income - expense;
 
-      // Calculate percentages
       const total = this.totalIncome + this.totalExpense;
       this.incomePercentage = total ? (this.totalIncome / total) * 100 : 0;
       this.expensePercentage = total ? (this.totalExpense / total) * 100 : 0;
 
-      // Bar Chart
       this.barChartLabels = ['Income', 'Expense'];
       this.barChartData = [{ data: [income, expense], label: 'Amount' }];
 
-      // Line Chart
-      const labels = data.map((expense) => expense.description);
-      this.lineChartLabels = labels;
-      this.lineChartData[0].data = incomeData.map((expense) => expense.amount);
-      this.lineChartData[1].data = expenseData.map((expense) => expense.amount);
+      // doughNut Chart
+      const doughnutLabels = data.map((expense) => expense.category);
+      this.doughnutChartLabels = doughnutLabels.filter(
+        (label, index) => index < 5
+      );
+      this.doughnutChartData[0].data = incomeData.map(
+        (expense) => expense.amount
+      );
+      this.doughnutChartData[1].data = expenseData.map(
+        (expense) => expense.amount
+      );
 
       // Pie Chart
       this.pieChartLabels = ['Income', 'Expense'];
       this.pieChartData[0].data = [income, expense];
+
+      data.forEach((entry) => {
+        const date = new Date(entry.date);
+        const month = `${date.getFullYear()}-${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, '0')}`;
+        const monthF = new Intl.DateTimeFormat('en-US', {
+          month: 'short',
+          year: 'numeric',
+        }).format(date);
+
+        if (!this.monthlyData[monthF]) {
+          this.monthlyData[monthF] = { income: 0, expense: 0 };
+        }
+        if (entry.type === 'income') {
+          this.monthlyData[monthF].income += entry.amount;
+        } else {
+          this.monthlyData[monthF].expense += entry.amount;
+        }
+      });
+      const lineLabels = Object.keys(this.monthlyData).reverse();
+      const incomeLineData = lineLabels.map(
+        (label) => this.monthlyData[label].income
+      );
+      const expenseLineData = lineLabels.map(
+        (label) => this.monthlyData[label].expense
+      );
+
+      this.lineChartLabels = lineLabels;
+      this.lineChartData[0].data = incomeLineData;
+      this.lineChartData[1].data = expenseLineData;
+      console.log(
+        this.lineChartData,
+        this.lineChartLabels,
+        this.lineChartOptions
+      );
     });
   }
 }
